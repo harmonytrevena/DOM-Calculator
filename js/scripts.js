e169e069361afbb4d3eef43517ab74031e3087bc
Harmony Trevena <harmonytrevena@gmail.com>
2020-08-20 15:17:37 -0400

    Updated to allow addition, subtraction, multiplication and division.

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,12 @@
 'use strict';
 
-// const calculator = {
-//     displayValue: '0',
-//     firstOperand: null,
-//     waitingForSecondOperand: false,
-//     operator: null,
-//   };
-  
-//   function inputDigit(digit) {
-//     const { displayValue } = calculator;
-//     // Overwrite 'displayValue' if the current value is '0' otherwise append to it
-//     calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
-//   }
-  
-//   function updateDisplay() {
-//     const display = document.querySelector('.input');
-//     display.value = calculator.displayValue;
-//   }
-  
-//   updateDisplay();
-  
-//   const keys = document.querySelector('.calculator-keys');
-//   keys.addEventListener('click', function (event) {
-//     const { target } = event;
-//     if (!target.matches('button')) {
-//       return;
-//     }
-
-//     if (target.classList.contains('operator')) {
-//       console.log('operator', target.value);
-//       return;
-//     }
-  
-//     if (target.classList.contains('decimal')) {
-//       console.log('decimal', target.value);
-//       return;
-//     }
-  
-//     if (target.classList.contains('clear')) {
-//       console.log('clear', target.value);
-//       return;
-//     }
-  
-//     inputDigit(target.value);
-//     updateDisplay();
-//   });
-
 const input = document.getElementById('input'),
     numbers = document.querySelectorAll('.numbers'), // number button 
     operators = document.querySelectorAll('.operator'), // operator buttons
     result = document.querySelector('.equal'), // equal button
     reset = document.querySelector('.clear'); // clear button
 
-let resultDisplayed = false; // flag to keep and eye on what output is displayed
+let resultDisplayed = false; // flag to keep an eye on what output is displayed
 
 let numberOperatorsArray = [];
 
@@ -60,7 +14,6 @@ numbers.forEach(function (number){
     number.addEventListener('click', function (){
         input.innerHTML += this.value;
         numberOperatorsArray = [...numberOperatorsArray, this.value];
-        console.log(numberOperatorsArray);
     });
 });
 
@@ -68,14 +21,59 @@ operators.forEach(function (operator){
     operator.addEventListener('click', function (){
         input.innerHTML += this.value;
         numberOperatorsArray = [...numberOperatorsArray, this.value];
-        console.log(numberOperatorsArray);
     });
 });
 
 result.addEventListener('click', function (){
         // this will do math
+        let numbersString = "";
+        let resultArray = [];
+        for (let char of numberOperatorsArray) {
+            let numReg = /\d/; // this is a regex (regular expressions)
+            if (numReg.test(char) || char === '.') {
+                numbersString += char;
+            } else {
+                resultArray = [...resultArray, Number(numbersString), char];
+                // reset here to stop concatenating
+                numbersString = "";
+            }
+        }
+        resultArray = [...resultArray, Number(numbersString)];
+        
+        // used for multiplication
+        let multiply = resultArray.indexOf("*");
+        while (multiply !== -1) {
+            resultArray.splice(multiply-1, 3, resultArray[multiply - 1] * resultArray[multiply + 1]);
+            multiply = resultArray.indexOf("*");
+        }
+
+        // used for division
+        let divide = resultArray.indexOf("/");
+        while (divide !== -1) {
+            resultArray.splice(divide-1, 3, resultArray[divide - 1] * resultArray[divide + 1]);
+            divide = resultArray.indexOf("/");
+        }
+
+        // used for addition
+        let add = resultArray.indexOf("+");
+        while (add !== -1) {
+            resultArray.splice(add-1, 3, resultArray[add - 1] + resultArray[add + 1]);
+            add = resultArray.indexOf("+");
+        }
+        
+        // used for subtraction
+        let subtract = resultArray.indexOf("-");
+        while (subtract !== -1) {
+            resultArray.splice(subtract-1, 3, resultArray[subtract - 1] - resultArray[subtract + 1]);
+            subtract = resultArray.indexOf("-");
+        }
+        // result
+        numberOperatorsArray = [...resultArray];
+        input.innerHTML = numberOperatorsArray[0];
     });
 
-reset.addEventListener('click', function (){
+    // clearing the input on the press of C
+    reset.addEventListener('click', function (){
+        numberOperatorsArray = [];
         input.innerHTML = "";
     });
